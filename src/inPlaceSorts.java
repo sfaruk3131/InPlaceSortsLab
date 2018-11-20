b347d1a7b7b334af633e22121b736ac1d888c204
BT_1N3_23@bktech.local <BT_1N3_23@bktech.local>
2018-11-20 10:14:08 -0500

    Initial commit

---
+++ src/inPlaceSorts.java
@@ -0,0 +1,16 @@
+public class inPlaceSorts
+{
+    public static void insertionSort(int [] list1)
+    {
+
+    }
+    public static void selectionSort(double[] list1)
+    {
+
+    }
+    public static void bubbleSort(String[] list1)
+    {
+
+    }
+
+}
